6ea86fdfc119211dcdb9144e35d816ea8bbda56b
rkay <rafey@email.com>
2021-02-18 19:40:44 +0500

    delete other users messages

--- bot.py
+++ bot.py
@@ -22,6 +22,12 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="ourWorld die."))
     print(f"{bot.user} has connected to Discord.")
 
+@bot.event
+async def on_message(msg):
+    if msg.author == bot.user:
+        return
+    await bot.delete_message(msg)
+
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title="ModBot - Help", colour=discord.Colour(0x1e041f), description="ModBot is a basic ourWorld bot that features a collection of unpatched exploits in the game. The bot has its own accounts to interact with the game and it does not get linked with the target account in any way. The features provided here are only a Proof-of-Concept of unpatched glitches.")
@@ -33,6 +39,8 @@ async def help(ctx):
     embed.add_field(inline=False,name="```!locate <ign>``` ```!l <ign>```", value=" ```Locate an ourWorld user```")
 
     await ctx.send(embed=embed)
+
+
 
 
 bot.run(TOKEN)
